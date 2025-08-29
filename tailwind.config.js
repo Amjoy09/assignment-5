1ffb2dc3dcbab2eed8e8de729acd4dc7637d4f9d
Amjoy09 <amjoy1125@gmail.com>
2025-08-29 16:13:18 +0600

    completed the hero section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        hind: ['"Hind Madurai"', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
